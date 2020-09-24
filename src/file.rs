6737d2b9950389c222af744d9b3a4a76bfbe247d
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2020-09-24 17:27:14 +0200

    backup commit

--- src/file.rs
+++ src/file.rs
@@ -1,15 +1,18 @@
 use std::fs::File;
 use std::io::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::process::Command;
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub name: String,
     pub enabled: bool,
-    pub dev_branch: String,
-    pub dev_hash: String,
-    pub master_branch: String,
+    pub feature_branch: String,
+    pub feature_hash: String,
+    pub target_branch: String,
+    pub target_hash: String,
     pub master_hash: String,
+    pub master_branch: String,
     pub continuous_integration: bool,
     pub continuous_delivery: bool,
     pub path: String,
@@ -25,12 +28,13 @@ pub fn read_conf() -> Config {
     return data;
 }
 
-pub fn read_dev_hash(config: &Config) -> bool {
-    let path = format!("{}{}{}", &config.path, "/.git/refs/heads/", &config.dev_branch);
+
+pub fn read_feature_hash(config: &Config) -> bool {
+    let path = format!("{}{}{}", &config.path, "/.git/refs/heads/", &config.feature_branch);
     let mut file = File::open(path).expect("Could not read file");
     let mut content = String::new();
     file.read_to_string(&mut content).expect("Could not read file.");
-    if content.trim() == config.dev_hash {
+    if content.trim() == config.feature_hash {
         return true;
     } else {
         return false;
@@ -38,14 +42,31 @@ pub fn read_dev_hash(config: &Config) -> bool {
 }
 
 
-pub fn read_master_hash(config: &Config) -> bool {
-    let path = format!("{}{}{}", &config.path, "/.git/refs/heads/", &config.master_branch);
+pub fn read_target_hash(config: &Config) -> bool {
+    let path = format!("{}{}{}", &config.path, "/.git/refs/heads/", &config.target_branch);
     let mut file = File::open(path).expect("Could not read file");
     let mut content = String::new();
     file.read_to_string(&mut content).expect("Could not read file.");
-    if content.trim() == config.master_hash {
+    println!("{}", config.path);
+    if content.trim() == config.target_hash {
         return true;
     } else {
         return false;
     }
 }
+
+
+pub fn merge_branch(feature_branch: &String, target_branch: &String) {
+    // git -C path checkout feature_branch && git merge master_branch && run tests 
+    let mut shell = Command::new("bash");
+    let command = format!("git checkout", );
+    shell
+        .arg("-c")
+        .arg("/home/mitchell/testy/target/debug/testy");
+    let o = String::from_utf8(shell.output().expect("Couldnt read std in/err").stdout).unwrap();
+    println!("{:#?}", o.trim());
+
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     let t = file::read_conf();
     file::read_dev_hash(&t);
     file::read_master_hash(&t);
-
+    // file::merge_branch();
 }
