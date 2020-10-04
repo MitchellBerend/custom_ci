0633f4ca0da72337c4d0b625f77889b24d1c01aa
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2020-10-04 15:08:41 +0200

    Added api skeleton.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,83 @@
-mod file;
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
 
-fn main() {
-    let t = file::read_conf();
-    file::read_dev_hash(&t);
-    file::read_master_hash(&t);
-    // file::merge_branch();
+
+// index
+// Created so the landing page exists and can direct to other endpoints.
+//
+// WIP
+// 
+//
+#[get("/")]
+fn index() -> String {
+    format!("Landing page.")
 }
+
+
+// pipeline
+// Endpoint for creating new and managing existing pipelines.
+//
+//
+#[get("/")]
+fn get_pipeline() ->  String { //name: String, path: String, master_branch: String, master_hash: String, feature_branch: String, feature_hash: String, test_path: String) -> String {
+    // Takes 7 argumetns, all strings.
+    // Adds pipeline model instance to database
+    format!("Not implemented yet.")
+}
+
+
+#[post("/")]
+fn post_pipeline() ->  String { //name: String, path: String, master_branch: String, master_hash: String, feature_branch: String, feature_hash: String, test_path: String) -> String {
+    // Takes 7 argumetns, all strings.
+    // Adds pipeline model instance to database
+    format!("Not implemented yet.")
+}
+
+
+#[put("/")]
+fn put_pipeline() -> String {
+    format!("Not implemented yet.")
+}
+
+
+#[delete("/")]
+fn delete_pipeline() -> String {
+    format!("Not implemented yet.")
+}
+
+
+// see current piplines
+//
+#[get("/")]
+fn get_current_pipelines() -> String {
+    format!("Not implemented yet.")
+}
+
+
+// check current pipeline status
+//
+#[get("/")]
+fn get_current_status() -> String {
+    format!("Not implemented yet.")
+}
+
+
+//rebuild pipeline
+//
+#[post("/")]
+fn post_rebuild() -> String {
+    format!("Not implemented yet.")
+}
+
+
+// main
+// 
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/pipeline", routes![get_pipeline, post_pipeline, put_pipeline, delete_pipeline])
+        .mount("/manage", routes![get_current_pipelines])
+        .mount("/rebuild", routes![post_rebuild]) 
+        .launch();
+}
+
